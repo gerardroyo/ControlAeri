a608fabff89470ef8ea840f6836d7d863202cbb2
Gerard Royo <gerard.royo@mataro.epiaedu.cat>
2019-11-19 15:24:27 +0100

    Initial commit

---
+++ src/com/ControlAeri/AirPlane.java
@@ -0,0 +1,83 @@
+package com.ControlAeri;
+
+public class AirPlane {
+    private String brand;
+    private String model;
+    private String licensePlate;
+    private String origin;
+    private String destination;
+    private int passengerCapacity;
+    private int crew;
+    private float velocity;
+    private float autonomy;
+    private float route;
+    private boolean undercarriage;
+    private Coordinate coordinate;
+
+    public AirPlane(String brand, String model, String licensePlate, String origin, String destination, int passengerCapacity, int crew, float velocity, float autonomy, float route, boolean undercarriage, int x, int y, int z) {
+        this.brand = brand;
+        this.model = model;
+        this.licensePlate = licensePlate;
+        this.origin = origin;
+        this.destination = destination;
+        this.passengerCapacity = passengerCapacity;
+        this.crew = crew;
+        this.velocity = velocity;
+        this.autonomy = autonomy;
+        this.route = route;
+        this.undercarriage = undercarriage;
+        this.coordinate = new Coordinate(x, y, z);
+    }
+
+    public void canviarMoviment(float) {
+
+    }
+
+    public String getBrand() { return brand; }
+
+    public void setBrand(String brand) { this.brand = brand; }
+
+    public String getModel() { return model; }
+
+    public void setModel(String model) { this.model = model; }
+
+    public String getLicensePlate() { return licensePlate; }
+
+    public void setLicensePlate(String licensePlate) { this.licensePlate = licensePlate; }
+
+    public String getOrigin() { return origin; }
+
+    public void setOrigin(String origin) { this.origin = origin; }
+
+    public String getDestination() { return destination; }
+
+    public void setDestination(String destination) { this.destination = destination; }
+
+    public int getPassengerCapacity() { return passengerCapacity; }
+
+    public void setPassengerCapacity(int passengerCapacity) { this.passengerCapacity = passengerCapacity; }
+
+    public int getCrew() { return crew; }
+
+    public void setCrew(int crew) { this.crew = crew; }
+
+    public float getVelocity() { return velocity; }
+
+    public void setVelocity(float velocity) { this.velocity = velocity; }
+
+    public float getAutonomy() { return autonomy; }
+
+    public void setAutonomy(float autonomy) { this.autonomy = autonomy; }
+
+    public float getRoute() { return route; }
+
+    public void setRoute(float route) { this.route = route; }
+
+    public boolean isUndercarriage() { return undercarriage; }
+
+    public void setUndercarriage(boolean undercarriage) { this.undercarriage = undercarriage; }
+
+    public Coordinate getCoordinate() { return coordinate; }
+
+    public void setCoordinate(Coordinate coordinate) { this.coordinate = coordinate; }
+}
---
+++ src/com/ControlAeri/Main.java
@@ -0,0 +1,8 @@
+package com.ControlAeri;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+    }
+}
